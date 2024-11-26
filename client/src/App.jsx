2b0a8bb8a43f19091eba4eb0502cfbcb463e257f
JacScript <jacksonsylas59@gmail.com>
2024-11-26 08:29:53 +0300

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-export default function App() {
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Banner2 from "./components/Banner/Banner2";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <main className="">
+      {/* <main className="overflow-x-hidden bg-white text-dark"> */}
+      <Hero />
+      <Services />
+      <Banner />
+      <Subscribe />
+      <Banner2 />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
